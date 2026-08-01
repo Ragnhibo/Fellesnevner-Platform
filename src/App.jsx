ff682fafffcabaaea4f4c2e860075e50f0bb1328
Ragis <ragnhild.anker.bohlin@gmail.com>
2026-08-01 12:36:11 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { GLOBAL_CSS } from "./theme";
+import Home from "./pages/Home";
+import Ordspill from "./games/Ordspill";
+import Ordjakt from "./games/Ordjakt";
+import Kodejakt from "./games/Kodejakt";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <style>{GLOBAL_CSS}</style>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ordspill" element={<Ordspill />} />
+        <Route path="/ordjakt" element={<Ordjakt />} />
+        <Route path="/kodejakt" element={<Kodejakt />} />
+      </Routes>
+      <Analytics />
+    </BrowserRouter>
+  );
+}
